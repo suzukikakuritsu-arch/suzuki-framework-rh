b550c559585f793a5843170be4bdd5d4a39119a4
Yukiya Suzuki <suzuki.kakuritsu@gmail.com>
2026-04-19 03:41:26 +0900

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,11 +1,14 @@
 import Lake
 open Lake DSL
 
-package «suzuki-rh» where
-  srcDir := "."
+package "suzuki-rh" where
+  leanOptions := #[⟨`pp.unicode.fun, true⟩, ⟨`autoImplicit, false⟩]
 
+-- Mathlibを安定したコミットに固定
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.11.0"
 
+@[default_target]
 lean_lib SuzukiRH where
   srcDir := "."
+  roots := #[`SuzukiRH]
