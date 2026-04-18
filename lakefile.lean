5093836b3be1054da646c175587bcb0032bc0d37
Yukiya Suzuki <suzuki.kakuritsu@gmail.com>
2026-04-19 02:53:01 +0900

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,19 +1,12 @@
 import Lake
 open Lake DSL
 
--- パッケージ名はリポジトリ名に合わせる必要はありませんが、
--- ここでは汎用的な名称にします
-package «suzuki-rh» where
-  leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩,
-    ⟨`autoImplicit, false⟩
-  ]
+package "suzuki-rh" where
+  leanOptions := #[⟨`pp.unicode.fun, true⟩, ⟨`autoImplicit, false⟩]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
--- 【ここが重要】フォルダ名が SuzukiRH なら、ここも SuzukiRH にする
 @[default_target]
 lean_lib SuzukiRH where
   srcDir := "."
-  roots := #[`SuzukiRH]
