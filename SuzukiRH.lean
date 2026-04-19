255a84d26ef5f040747677894e8cb4c4c8a7b84b
Yukiya Suzuki <suzuki.kakuritsu@gmail.com>
2026-04-19 09:52:56 +0900

    Update SuzukiRH.lean

--- SuzukiRH.lean
+++ SuzukiRH.lean
@@ -3,4 +3,6 @@ import SuzukiRH.ZetaProperties
 import SuzukiRH.FunctionalEquation
 import SuzukiRH.Symmetry
 import SuzukiRH.GroupAction
+import SuzukiRH.Orbit
+import SuzukiRH.Rigidity
 import SuzukiRH.MainTheorem
